d90e4e3373dda182d672818fc2f87417d8a0a682
Eli Rykoff <erykoff@stanford.edu>
2026-09-21 13:23:35 -0700

    Add parallelization for tests run via meson.

--- build-cc-release.sh
+++ build-cc-release.sh
@@ -1,5 +1,5 @@
 NJOBS="${EUPSPKG_NJOBS:-1}"
 
 # Compile, test and install C++ library
-meson compile -C build-release -j $NJOBS && meson test -C build-release && meson install -C build-release
+meson compile -C build-release -j $NJOBS && MESON_NUM_PROCESSES=$NJOBS meson test -C build-release && MESON_NUM_PROCESSES=$NJOBS meson install -C build-release
 
--- build-py-release.sh
+++ build-py-release.sh
@@ -3,4 +3,4 @@ NJOBS="${EUPSPKG_NJOBS:-1}"
 # Compile, test and install python release libraries
 # Unfortunately, the library must be installed before any python tests are run
 # (otherwise the import will fail)
-meson compile -C python/build-release -j $NJOBS && meson install -C python/build-release && meson test -C python/build-release
+meson compile -C python/build-release -j $NJOBS && MESON_NUM_PROCESSES=$NJOBS meson install -C python/build-release && MESON_NUM_PROCESSES=$NJOBS meson test -C python/build-release
