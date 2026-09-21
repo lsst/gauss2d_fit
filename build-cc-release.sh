06b8c23d169aa3478e0002e3b0bc0b9c39110647
Eli Rykoff <erykoff@stanford.edu>
2026-09-21 09:27:28 -0700

    Use multi-core compiling if configured to do so.

--- build-cc-release.sh
+++ build-cc-release.sh
@@ -1,3 +1,5 @@
+NJOBS="${EUPSPKG_NJOBS:-1}"
+
 # Compile, test and install C++ library
-meson compile -C build-release && meson test -C build-release && meson install -C build-release
+meson compile -C build-release -j $NJOBS && meson test -C build-release && meson install -C build-release
 
--- build-py-release.sh
+++ build-py-release.sh
@@ -1,5 +1,6 @@
+NJOBS="${EUPSPKG_NJOBS:-1}"
+
 # Compile, test and install python release libraries
 # Unfortunately, the library must be installed before any python tests are run
 # (otherwise the import will fail)
-meson compile -C python/build-release && meson install -C python/build-release && meson test -C python/build-release
-
+meson compile -C python/build-release -j $NJOBS && meson install -C python/build-release && meson test -C python/build-release
